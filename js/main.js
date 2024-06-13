fb5be80844c7f1e25cebf0e483aebc1cfe8e2eb8
Preston Chapman <pchapman82070@uat.edu>
2024-06-13 10:44:14 -0700

    Add Game

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
+var games = []
+
 document.addEventListener("DOMContentLoaded", () => {
     const REMOVE_FORM = {
         element: document.getElementById("remove"),
-        input: document.getElementById("selectedGame")
+        inputs: {
+            dropGame: document.getElementById("selectedGame")
+        }
     }
     const ADD_FORM = {
         element: document.getElementById("add"),
-        input: document.getElementById("inputGame")
+        input: {
+           textGame: document.getElementById("inputGame")
+        }
     }
 
     REMOVE_FORM.element.addEventListener("submit", (e) => {
+        e.preventDefault();
         alert("Removed")
     })
     ADD_FORM.element.addEventListener("submit", (e) => {
+        e.preventDefault();
         alert("Added")
+        games.push(ADD_FORM.input.textGame.value);
+        console.log(games)
     })
 })
