fa2253ea164a9bd6e13e24b8f5615f2503ea8deb
Preston Chapman <pchapman82070@uat.edu>
2024-06-15 14:42:31 -0700

    Covert to functions
    Changed code to use functions for each action

--- script/main.js
+++ script/main.js
@@ -1,7 +1,38 @@
 var games = []
+var REMOVE_FORM;
+var ADD_FORM;
 
-document.addEventListener("DOMContentLoaded", () => {
-    const REMOVE_FORM = {
+document.addEventListener("DOMContentLoaded", onLoad)
+
+function removeGame(e){
+    e.preventDefault();
+    alert("Removed")
+    let index = REMOVE_FORM.inputs.dropGame.value
+    console.log(index)
+    games.splice(index, 1)
+    REMOVE_FORM.update();
+}
+
+function addGame(e){
+    let value = ADD_FORM.input.textGame.value
+    e.preventDefault();
+    alert("Added")
+    games.push(value);
+    games.sort();
+    console.log(games)
+    ADD_FORM.input.textGame.value = ""
+    REMOVE_FORM.update();
+}
+
+function resetList(){
+    alert("Reset List");
+    games = []
+    REMOVE_FORM.update();
+    console.log(games)
+}
+
+function onLoad(){
+    REMOVE_FORM = {
         element: document.getElementById("remove"),
         inputs: {
             dropGame: document.getElementById("selectedGame")
@@ -16,36 +47,16 @@ document.addEventListener("DOMContentLoaded", () => {
             });
         }
     }
-    const ADD_FORM = {
+    ADD_FORM = {
         element: document.getElementById("add"),
         input: {
            textGame: document.getElementById("inputGame")
         }
     }
+    
     const RESET_BTN = document.getElementById("reset");
 
-    REMOVE_FORM.element.addEventListener("submit", (e) => {
-        e.preventDefault();
-        alert("Removed")
-        let index = REMOVE_FORM.inputs.dropGame.value
-        console.log(index)
-        games.splice(index, 1)
-        REMOVE_FORM.update();
-    })
-    ADD_FORM.element.addEventListener("submit", (e) => {
-        let value = ADD_FORM.input.textGame.value
-        e.preventDefault();
-        alert("Added")
-        games.push(value);
-        games.sort();
-        console.log(games)
-  
-        REMOVE_FORM.update();
-    })
-    RESET_BTN.addEventListener("click", () => {
-        alert("Reset List");
-        games = []
-        REMOVE_FORM.update();
-        console.log(games)
-    })
-})
+    REMOVE_FORM.element.addEventListener("submit", (e) => removeGame(e))
+    ADD_FORM.element.addEventListener("submit",(e) => addGame(e))
+    RESET_BTN.addEventListener("click",(e) => resetList(e))
+}
